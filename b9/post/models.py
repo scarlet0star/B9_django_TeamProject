2d094e0a97fa2c21f60d730568c7dd33317b0103
banghyunjae <banghyunjae@naver.com>
2023-04-12 10:59:27 +0900

    feed

--- b9/post/models.py
+++ b9/post/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from user.models import User
+from django.utils import timezone
 
 # Create your models here.
+class PostModel(models.Model):
+    class Meta:
+        db_table = "Post"
+
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    post = models.TextField()
+    photo = models.ImageField(blank=True, upload_to="media/%Y/%m/%d")
+    like_users = models.ManyToManyField(User, related_name='like_articles')
+    like_count = models.IntegerField(blank=True, null=True,default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    contant = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
--- b9/post/views.py
+++ b9/post/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import PostModel, Comment, Like
+from user.models import Profile, User
+from .forms import PostForm
+from django.http import HttpResponse
 
 # Create your views here.
+
+# 메인 페이지
+def home(request):
+    posts = PostModel.objects.all().order_by('-created_at')
+    likes = Like.objects.all()
+    return render (request, 'post/home.html', {'posts': posts, 'likes':likes})
+
+
+# 글 작성 view
+@login_required
+def post_create(request):
+    if request.method == 'GET':
+        form = PostForm()
+        return render(request, 'post/post_create.html',{'form': form})
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            post = PostModel(
+                writer = user,
+                title = form.cleaned_data.get('title'),
+                post = form.cleaned_data.get('post'),
+                photo = form.changed_data.get('photo'),
+            )
+            post.save()
+            return redirect('/post')
+
+# 게시글 삭제
+def post_delete(request, post_id):
+    post = PostModel.objects.get(id=post_id)
+    post.delete()
+    return redirect('/post')
+
+#댓글 추가
+@login_required 
+def add_comment(request,post_id):
+    post = get_object_or_404(PostModel,pk=post_id)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        author = request.user 
+        comment = Comment.objects.create(
+            content=content,
+            author=author,
+            post=post
+        )
+        return redirect('post_detail', post_id=post.id)
+    else:
+        return redirect('post_list')
+
+
+#게시물에 대한 좋아요를 토글하는 함수
+@login_required
+def toggle_like(request, post_id):
+    if request.user.is_authenticated:
+        post = get_object_or_404(PostModel, pk=post_id)
+        user =request.user
+        if post.like_users.filter(pk=user.pk).exists():
+            post.like_users.remove(user)
+            post.like_count -= 1
+            post.save()
+        else:
+            post.like_users.add(user)
+            post.like_count += 1
+            post.save()
+        return redirect('/post')
+    return redirect('/user/login')
+
+
+# #현재 사용자가 작성한 게시글에 대한 좋아요 알림을 보여주는 함수
+# @login_required
+# def like_notificatons(request):
+#     likes = Like.objects.filter(post__writer=request.user).order_by('-created_at')
+#     context = {'likes':likes}
+#     return render(request, 'post/like_notifications.html', context)
+
+def all_delete(request):
+    PostModel.objects.all().delete()
+    return redirect('/post')
+
