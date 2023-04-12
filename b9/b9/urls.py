b5699f4932aac36e3c9cb8d1f755985a8decfa30
Yoon joonyeol <raoneli@naver.com>
2023-04-12 13:58:45 +0900

    merge update

--- b9/b9/urls.py
+++ b9/b9/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('user/', include('user.urls')),
+    path('post/', include('post.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- b9/post/models.py
+++ b9/post/models.py
@@ -4,14 +4,14 @@ from user.models import User
 from django.utils import timezone
 
 # Create your models here.
-class PostModel(models.Model):
+class Post(models.Model):
     class Meta:
         db_table = "Post"
 
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
     post = models.TextField()
-    photo = models.ImageField(blank=True, upload_to="media/%Y/%m/%d")
+    photo = models.ImageField("포스트이미지",upload_to="post_photo/%Y/%m/%d" , blank=True, null=True)
     like_users = models.ManyToManyField(User, related_name='like_articles')
     like_count = models.IntegerField(blank=True, null=True,default=0)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,13 +21,13 @@ class PostModel(models.Model):
 class Comment(models.Model):
     contant = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Like(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
