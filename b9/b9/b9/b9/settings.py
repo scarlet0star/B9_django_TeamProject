9b4abcca36e52045b90259841b50c04bba5bd66b
banghyunjae <banghyunjae@naver.com>
2023-04-12 10:25:49 +0900

    sd

--- b9/b9/b9/b9/settings.py
+++ b9/b9/b9/b9/settings.py
@@ -132,10 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SITE_ID = 1
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = 'media/'
-
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
         'SCOPE': [
